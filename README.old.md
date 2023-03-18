4ab91c1c2874e3a57d534e344544184a9403e121
= t3nt4c135 <= j.norm911@gmail.com>
2023-03-18 12:14:00 -0700

    quick route fix

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# crud4react
-CRUD Operations using react.js, axios, mockapi and more.
--- src/components/Router/AppRouter.jsx
+++ src/components/Router/AppRouter.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import Homepage from '../Homepage/Homepage';
-import Signup from '../Auth/Signup/Signup';
+import Signup from '../auth/Signup/Signup';
 import AdminDashboard from '../admin/AdminDashboard';
 
 function AppRouter() {
